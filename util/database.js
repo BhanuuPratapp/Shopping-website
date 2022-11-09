f0d962beeee8ca7fc25a60f1509dc1907876fa84
Bhanu Pratap <pratapbhanuuu@gmail.com>
2022-11-09 14:36:24 +0530

    third commit

--- util/database.js
+++ util/database.js
@@ -1,9 +1,10 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
     password: 'Bhanu@123'
-})
+});
 
 module.exports = pool.promise();
